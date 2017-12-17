dbaba8bf2fb8e680d53b33cefa87623323dbc1a1
David Samuel <cryptondave@gmail.com>
2017-12-16 16:43:56 -0800

    added props to Person class

--- js/index.js
+++ js/index.js
@@ -1,2 +1,9 @@
-require('babel-polyfill');
+// js/components/index.js
+var React = require('react');
+var ReactDOM = require('react-dom');
 
+var PersonList = require('./components/person-list');
+
+document.addEventListener('DOMContentLoaded', function() {
+    ReactDOM.render(<PersonList />, document.getElementById('app'));
+});
